b7bc9f3b3d0408e9685a70c01a301e0d03eb7f40
Paul Sanford <me@paulsanford.net>
2022-04-01 22:00:09 -0500

    Jumps, subroutines, index, comp

--- src/lib.rs
+++ src/lib.rs
@@ -40,31 +40,33 @@ fn u32_to_word(i: u32) -> Word {
 
 #[derive(FromPrimitive, Debug, Clone, Copy)]
 enum OpCode {
+    ADD = 0x18,  // x
+    AND = 0x40,  // x
+    COMP = 0x28, // x
+    DIV = 0x24,  // x
+    J = 0x3C,    // x
+    JEQ = 0x30,  // x
+    JGT = 0x34,  // x
+    JLT = 0x38,  // x
+    JSUB = 0x48, // x
+    LDA = 0x00,  // x
+    LDCH = 0x50, // x
+    LDL = 0x08,  // x
+    LDX = 0x04,  // x
+    MUL = 0x20,  // x
+
     OR = 0x44, // x
     RD = 0xD8,
-    RSUB = 0x4C,
+    RSUB = 0x4C, // x
     STA = 0x0C,  // x
     STCH = 0x54, // x
     STL = 0x14,  // x
     STSW = 0xE8, // x
     STX = 0x10,  // x
     SUB = 0x1C,  // x
-    ADD = 0x18,  // x
-    AND = 0x40,  // x
-    COMP = 0x28,
-    DIV = 0x24, // x
-    J = 0x3C,
-    JEQ = 0x30,
-    JGT = 0x34,
-    JLT = 0x38,
-    JSUB = 0x48,
-    LDA = 0x00,  // x
-    LDCH = 0x50, // x
-    LDL = 0x08,  // x
-    LDX = 0x04,  // x
-    MUL = 0x20,  // x
+
     TD = 0xE0,
-    TIX = 0x2C,
+    TIX = 0x2C, // x
     WD = 0xDC,
 }
 
@@ -121,7 +123,7 @@ impl Debug for Vm {
 }
 
 impl Vm {
-    fn empty() -> Self {
+    pub fn empty() -> Self {
         Self {
             memory: [0; 1 << 15],
             A: [0; 3],
@@ -157,8 +159,18 @@ impl Vm {
         u16::from_be_bytes([self.PC[1], self.PC[2]])
     }
 
+    fn comp(&mut self, register: Word, address: u16, indexed: bool) {
+        let memory = self.word_at(address, indexed);
+        match register.cmp(&memory) {
+            std::cmp::Ordering::Less => self.SW[2] = (self.SW[2] & 0xFC) | 0b0001,
+            std::cmp::Ordering::Equal => self.SW[2] = (self.SW[2] & 0xFC) | 0b0000,
+            std::cmp::Ordering::Greater => self.SW[2] = (self.SW[2] & 0xFC) | 0b0010,
+        }
+    }
+
     pub fn step(&mut self) {
         let op = Op::from_word(self.word_at(self.pc_address(), false)).unwrap();
+        self.PC = u32_to_word(self.PC.as_u32() + 3);
         match op.opcode {
             // Load/store
             OpCode::LDA => {
@@ -226,14 +238,47 @@ impl Vm {
                 let memory = self.word_at(op.address, op.indexed);
                 self.A = u32_to_word(self.A.as_u32().wrapping_div(memory.as_u32()));
             }
-            opcode => unimplemented!(r"Opcode {:?} not implemented", opcode),
-        }
 
-        match op.opcode {
-            // TODO: jumps etc shouldn't increment
-            _ => {
-                self.PC = u32_to_word(self.PC.as_u32() + 3);
+            // Comp
+            OpCode::COMP => {
+                self.comp(self.A, op.address, op.indexed);
             }
+
+            // Jumps
+            OpCode::J => {
+                self.PC = self.word_at(op.address, op.indexed);
+            }
+            OpCode::JLT => {
+                if self.SW[2] & 0b0011 == 0b0001 {
+                    self.PC = self.word_at(op.address, op.indexed);
+                }
+            }
+            OpCode::JEQ => {
+                if self.SW[2] & 0b0011 == 0b0000 {
+                    self.PC = self.word_at(op.address, op.indexed);
+                }
+            }
+            OpCode::JGT => {
+                if self.SW[2] & 0b0011 == 0b0010 {
+                    self.PC = self.word_at(op.address, op.indexed);
+                }
+            }
+
+            // Subroutines
+            OpCode::JSUB => {
+                self.L = self.PC;
+                self.PC = self.word_at(op.address, op.indexed);
+            }
+            OpCode::RSUB => {
+                self.PC = self.L;
+            }
+
+            // Index
+            OpCode::TIX => {
+                self.X = u32_to_word(self.X.as_u32() + 1);
+                self.comp(self.X, op.address, op.indexed);
+            }
+            opcode => unimplemented!(r"Opcode {:?} not implemented", opcode),
         }
     }
 }
@@ -276,6 +321,18 @@ mod test {
         vm.memory[address] = word[0];
         vm.memory[address + 1] = word[1];
         vm.memory[address + 2] = word[2];
+    }
+
+    fn ni_op(vm: &mut Vm, address: usize, opcode: OpCode, target: u16) {
+        set_op(
+            vm,
+            address,
+            Op {
+                opcode,
+                indexed: false,
+                address: target,
+            },
+        );
     }
 
     #[allow(dead_code)]
@@ -456,5 +513,159 @@ mod test {
         vm.step();
 
         assert_eq!(vm.A.as_i32(), 2);
+    }
+
+    #[test]
+    fn jumps() {
+        let mut vm = setup_op(OpCode::J, 99);
+        set_int(&mut vm, 99, 33);
+        ni_op(&mut vm, 33, OpCode::LDA, 99);
+
+        vm.step();
+        vm.step();
+
+        assert_eq!(vm.A.as_u32(), 33);
+
+        // Comp JEQ EQ
+        let mut vm = setup_op(OpCode::COMP, 99);
+        ni_op(&mut vm, 3, OpCode::JEQ, 99);
+        vm.A = u32_to_word(33);
+        set_int(&mut vm, 99, 33);
+        vm.step();
+        assert_eq!(vm.SW[2] & 0x03, 0);
+        vm.step();
+        assert_eq!(vm.PC.as_u32(), 33);
+
+        // Comp JEQ LT
+        let mut vm = setup_op(OpCode::COMP, 99);
+        ni_op(&mut vm, 3, OpCode::JEQ, 99);
+        vm.A = u32_to_word(33);
+        set_int(&mut vm, 99, 34);
+        vm.step();
+        assert_eq!(vm.SW[2] & 0x03, 1);
+        vm.step();
+        assert_eq!(vm.PC.as_u32(), 6);
+
+        // Comp JEQ GT
+        let mut vm = setup_op(OpCode::COMP, 99);
+        ni_op(&mut vm, 3, OpCode::JEQ, 99);
+        vm.A = u32_to_word(33);
+        set_int(&mut vm, 99, 32);
+        vm.step();
+        assert_eq!(vm.SW[2] & 0x03, 2);
+        vm.step();
+        assert_eq!(vm.PC.as_u32(), 6);
+
+        // Comp JLT
+        let mut vm = setup_op(OpCode::COMP, 99);
+        ni_op(&mut vm, 3, OpCode::JLT, 99);
+        vm.A = u32_to_word(33);
+        set_int(&mut vm, 99, 34);
+        vm.step();
+        assert_eq!(vm.SW[2] & 0x03, 1);
+        vm.step();
+        assert_eq!(vm.PC.as_u32(), 34);
+
+        // Comp JGT
+        let mut vm = setup_op(OpCode::COMP, 99);
+        ni_op(&mut vm, 3, OpCode::JGT, 99);
+        vm.A = u32_to_word(33);
+        set_int(&mut vm, 99, 32);
+        vm.step();
+        assert_eq!(vm.SW[2] & 0x03, 2);
+        vm.step();
+        assert_eq!(vm.PC.as_u32(), 32);
+    }
+
+    #[test]
+    fn subroutines() {
+        let mut vm = setup_op(OpCode::JSUB, 9);
+        ni_op(&mut vm, 99, OpCode::LDA, 199);
+        ni_op(&mut vm, 102, OpCode::RSUB, 0);
+        ni_op(&mut vm, 3, OpCode::STA, 6);
+        set_int(&mut vm, 199, 1234);
+        set_int(&mut vm, 9, 99);
+
+        vm.step();
+        vm.step();
+        vm.step();
+        vm.step();
+
+        assert_eq!(vm.word_at(6, false).as_u32(), 1234);
+    }
+
+    #[test]
+    fn index() {
+        let mut vm = Vm::empty();
+        set_op(
+            &mut vm,
+            0,
+            Op {
+                opcode: OpCode::ADD,
+                indexed: true,
+                address: 99,
+            },
+        );
+        ni_op(&mut vm, 3, OpCode::TIX, 96);
+        ni_op(&mut vm, 6, OpCode::TIX, 96);
+        ni_op(&mut vm, 9, OpCode::TIX, 96);
+        ni_op(&mut vm, 12, OpCode::JLT, 93);
+        set_int(&mut vm, 93, 0);
+        set_int(&mut vm, 96, 15);
+        set_int(&mut vm, 99, 1);
+        set_int(&mut vm, 102, 2);
+        set_int(&mut vm, 105, 3);
+        set_int(&mut vm, 108, 4);
+        set_int(&mut vm, 111, 5);
+
+        vm.step();
+        vm.step();
+        vm.step();
+        vm.step();
+        vm.step();
+
+        assert_eq!(vm.A.as_u32(), 1);
+        assert_eq!(vm.X.as_u32(), 3);
+        assert_eq!(vm.PC.as_u32(), 0);
+
+        vm.step();
+        vm.step();
+        vm.step();
+        vm.step();
+        vm.step();
+
+        assert_eq!(vm.A.as_u32(), 3);
+        assert_eq!(vm.X.as_u32(), 6);
+        assert_eq!(vm.PC.as_u32(), 0);
+
+        vm.step();
+        vm.step();
+        vm.step();
+        vm.step();
+        vm.step();
+
+        assert_eq!(vm.A.as_u32(), 6);
+        assert_eq!(vm.X.as_u32(), 9);
+        assert_eq!(vm.PC.as_u32(), 0);
+
+        vm.step();
+        vm.step();
+        vm.step();
+        vm.step();
+        vm.step();
+
+        assert_eq!(vm.A.as_u32(), 10);
+        assert_eq!(vm.X.as_u32(), 12);
+        assert_eq!(vm.PC.as_u32(), 0);
+
+        vm.step();
+        vm.step();
+        vm.step();
+        vm.step();
+        vm.step();
+
+        assert_eq!(vm.A.as_u32(), 15);
+        assert_eq!(vm.X.as_u32(), 15);
+        assert_eq!(vm.PC.as_u32(), 15);
     }
 }

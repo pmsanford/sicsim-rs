85498c557ed6857ef6e8ae0ae885515dc35feb18
Paul Sanford <me@paulsanford.net>
2022-04-01 23:18:59 -0500

    A couple modules

---
+++ src/op.rs
@@ -0,0 +1,65 @@
+use num_derive::FromPrimitive;
+
+use crate::word::Word;
+
+#[derive(FromPrimitive, Debug, Clone, Copy)]
+pub enum OpCode {
+    ADD = 0x18,
+    AND = 0x40,
+    COMP = 0x28,
+    DIV = 0x24,
+    J = 0x3C,
+    JEQ = 0x30,
+    JGT = 0x34,
+    JLT = 0x38,
+    JSUB = 0x48,
+    LDA = 0x00,
+    LDCH = 0x50,
+    LDL = 0x08,
+    LDX = 0x04,
+    MUL = 0x20,
+
+    OR = 0x44,
+    RD = 0xD8,
+    RSUB = 0x4C,
+    STA = 0x0C,
+    STCH = 0x54,
+    STL = 0x14,
+    STSW = 0xE8,
+    STX = 0x10,
+    SUB = 0x1C,
+
+    TD = 0xE0,
+    TIX = 0x2C,
+    WD = 0xDC,
+}
+
+#[derive(Debug)]
+pub struct Op {
+    pub opcode: OpCode,
+    pub indexed: bool,
+    pub address: u16,
+}
+
+impl Op {
+    pub fn from_word(word: Word) -> Option<Self> {
+        num::FromPrimitive::from_u8(word[0]).map(|opcode| Self {
+            opcode,
+            indexed: word[1] & 0b1000_0000 > 0,
+            address: u16::from_be_bytes([word[1] & 0x7F, word[2]]),
+        })
+    }
+}
+
+impl Into<Word> for Op {
+    fn into(self) -> Word {
+        let opcode = self.opcode as u8;
+        let [msb, lsb] = self.address.to_be_bytes();
+        let mut msb = msb & 0x7F;
+        if self.indexed {
+            msb = msb | 0x80;
+        }
+
+        [opcode, msb, lsb]
+    }
+}
---
+++ src/word.rs
@@ -0,0 +1,37 @@
+pub type Word = [u8; 3];
+
+pub trait WordExt {
+    fn as_u32(&self) -> u32;
+    fn as_usize(&self) -> usize;
+    fn as_i32(&self) -> i32;
+}
+
+impl WordExt for Word {
+    fn as_u32(&self) -> u32 {
+        let [a, b, c] = *self;
+        u32::from_be_bytes([0, a, b, c])
+    }
+
+    fn as_usize(&self) -> usize {
+        let [a, b, c] = *self;
+        usize::from_be_bytes([0, 0, 0, 0, 0, a, b, c])
+    }
+
+    fn as_i32(&self) -> i32 {
+        let [msb, mid, lsb] = *self;
+        let neg = msb & 0x80 > 0;
+        let msb = msb & 0x7F;
+        let val = u32::from_be_bytes([0, msb, mid, lsb]);
+        if neg {
+            // Don't do any twos complement just sign extend baybee
+            (val | 0xFF_80_00_00) as i32
+        } else {
+            val as i32
+        }
+    }
+}
+
+pub fn u32_to_word(i: u32) -> Word {
+    let [_, a, b, c] = i.to_be_bytes();
+    [a, b, c]
+}

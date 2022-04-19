985bb8e7db5cc96e1a6b7f2338f2dfffc93f12c7
Paul Sanford <me@paulsanford.net>
2022-04-18 22:51:29 -0500

    XE copy test

--- tests/xe.rs
+++ tests/xe.rs
@@ -70,3 +70,28 @@ fn test_devices() {
     assert_eq!(stop, StopReason::Halted);
     assert_eq!(*output_buffer.borrow_mut(), expected);
 }
+
+#[test]
+fn test_devices_xe() {
+    let test_program = include_str!("../programs/xe/copy_xe.ebj");
+    let test_harness = include_str!("../programs/xe/test_harness.ebj");
+    let mut vm = vm_with_program(test_harness);
+    load_program_at(&mut vm, test_program, 0);
+    println!(
+        "{:0>2X} {:0>2X} {:0>2X}",
+        vm.memory[0], vm.memory[1], vm.memory[2]
+    );
+    let input_device = FileInputDevice::new("tests/copy_input.txt").unwrap();
+    let contents = include_bytes!("copy_input.txt");
+    let mut expected = contents.to_vec();
+    expected.push('E' as u8);
+    expected.push('O' as u8);
+    expected.push('F' as u8);
+    let (output_buffer, output_device) = MemoryOutputDevice::new();
+    vm.add_device(Box::new(input_device), 0xF1);
+    vm.add_device(Box::new(output_device), 0x05);
+    let stop = vm.run_until(1000);
+    println!("buf len: {}", output_buffer.borrow().len());
+    assert_eq!(stop, StopReason::Halted);
+    assert_eq!(*output_buffer.borrow_mut(), expected);
+}

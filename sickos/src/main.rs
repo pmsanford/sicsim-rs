48fafa947bbaed558ab0df2634fd437cfc43bc15
Paul Sanford <me@paulsanford.net>
2023-04-18 17:35:23 -0600

    Dispatcher correctly alternating

--- sickos/src/main.rs
+++ sickos/src/main.rs
@@ -1,10 +1,28 @@
 use libsic::{
+    word::u32_to_word,
     xe::{
         load::{load_program_at, load_program_to},
+        op::AddressFlags,
         vm::{PrintlnDebugger, SicXeVm},
     },
     WordExt,
 };
+
+fn print_word_at(vm: &mut SicXeVm, caption: &str, address: u32) {
+    println!(
+        "{}: Byte at {:#08x} ({:#08}): {:#08x}",
+        caption,
+        address,
+        address,
+        vm.word_at(address).unwrap().as_u32()
+    );
+}
+
+const PSB_ONE_STATUS: u32 = 0x206;
+const PSB_TWO_STATUS: u32 = 0x227;
+const ITERATOR_COUNTER: u32 = 0x7E5;
+const MULTIPLIER_COUNTER: u32 = 0xBD6;
+const RUNNING_PTR: u32 = 0x203;
 
 fn main() {
     let mut vm = SicXeVm::empty();
@@ -33,22 +51,34 @@ fn main() {
 
     let multiplier = include_str!("bin/multiplier.ebj");
     load_program_at(&mut vm, multiplier, 3000);
+    print_word_at(&mut vm, "iterator status", PSB_ONE_STATUS);
+    print_word_at(&mut vm, "multiplier status", PSB_TWO_STATUS);
+    print_word_at(&mut vm, "iterator sw", PSB_ONE_STATUS + 3);
+    print_word_at(&mut vm, "multiplier sw", PSB_TWO_STATUS + 3);
+    print_word_at(&mut vm, "iterator counter", ITERATOR_COUNTER);
+    print_word_at(&mut vm, "multipler counter", MULTIPLIER_COUNTER);
 
     // Set interrupt timer at 10 sec
     vm.I = [0, 0, 10];
     // Start in dispatcher
     vm.set_pc(80);
+    // Set pointer to running program
+    vm.set_at(
+        RUNNING_PTR,
+        &AddressFlags::default(),
+        u32_to_word(PSB_ONE_STATUS),
+    )
+    .unwrap();
     // Need to start in privileged mode
     vm.SW[0] |= 0x80;
 
-    println!("Byte at: {:#08x}", [vm.memory[521], vm.memory[522], vm.memory[523]].as_u32());
     println!("[0, 2, 9] is {}", [0, 2, 9].as_u32());
 
     println!("Stopped: {:?}", vm.run_until(100000));
 
-    println!("Pointer: {:#08x}", [vm.memory[99], vm.memory[100], vm.memory[101]].as_u32());
-
-    println!("A: {}", vm.A.as_u32());
-
-    println!("Word at 0x20C: {}", vm.word_at(0x20C).unwrap().as_u32());
+    println!("X: {}", vm.X.as_u32());
+    print_word_at(&mut vm, "iterator status", PSB_ONE_STATUS);
+    print_word_at(&mut vm, "multiplier status", PSB_TWO_STATUS);
+    print_word_at(&mut vm, "iterator counter", ITERATOR_COUNTER);
+    print_word_at(&mut vm, "multipler counter", MULTIPLIER_COUNTER);
 }

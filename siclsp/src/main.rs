77aba9adeaf57d4ca38c7c3aae5141fbf4d42cf6
Paul Sanford <me@paulsanford.net>
2023-10-01 12:15:09 -0600

    Add tracking references

--- siclsp/src/main.rs
+++ siclsp/src/main.rs
@@ -2,6 +2,7 @@ use std::collections::HashMap;
 
 use anyhow::Result;
 use once_cell::sync::Lazy;
+use rangemap::RangeMap;
 use sicasm2::parser::{self, Label, ProgramLine, Value};
 use tokio::sync::Mutex;
 #[allow(unused_imports)]
@@ -43,70 +44,21 @@ static MOD_MAP: Lazy<HashMap<SemanticTokenModifier, u32>> = Lazy::new(|| {
 struct Lsp {
     client: Client,
     text: Mutex<Option<String>>,
+    label_usage_map: Mutex<RangeMap<u32, u32>>,
+    label_reference_map: Mutex<RangeMap<u32, Vec<u32>>>,
+    semantic_tokens: Mutex<Vec<SemanticToken>>,
 }
 
-#[tower_lsp::async_trait]
-impl LanguageServer for Lsp {
-    async fn initialize(
-        &self,
-        _: InitializeParams,
-    ) -> tower_lsp::jsonrpc::Result<InitializeResult> {
-        let mut token_types = TYPE_MAP.iter().collect::<Vec<_>>();
-        token_types.sort_by_key(|(_, v)| *v);
-        let token_types = token_types
-            .iter()
-            .map(|(k, _)| (*k).clone())
-            .collect::<Vec<_>>();
-        let mut token_modifiers = MOD_MAP.iter().collect::<Vec<_>>();
-        token_modifiers.sort_by_key(|(_, v)| *v);
-        let token_modifiers = token_modifiers
-            .iter()
-            .map(|(k, _)| (*k).clone())
-            .collect::<Vec<_>>();
-        let tokens =
-            SemanticTokensServerCapabilities::SemanticTokensOptions(SemanticTokensOptions {
-                work_done_progress_options: WorkDoneProgressOptions::default(),
-                legend: SemanticTokensLegend {
-                    token_types,
-                    token_modifiers,
-                },
-                range: None,
-                full: Some(SemanticTokensFullOptions::Bool(true)),
-            });
-        let capabilities = ServerCapabilities {
-            semantic_tokens_provider: Some(tokens),
-            text_document_sync: Some(TextDocumentSyncCapability::Kind(TextDocumentSyncKind::FULL)),
-            ..ServerCapabilities::default()
-        };
-        let result = InitializeResult {
-            capabilities,
-            ..InitializeResult::default()
-        };
-        Ok(result)
-    }
-
-    async fn did_open(&self, params: DidOpenTextDocumentParams) {
-        let mut text = self.text.lock().await;
-        *text = Some(params.text_document.text);
-    }
-
-    async fn did_change(&self, params: DidChangeTextDocumentParams) {
-        let mut text = self.text.lock().await;
-        *text = Some(params.content_changes[0].text.clone())
-    }
-
+impl Lsp {
     #[allow(unused_assignments)]
-    async fn semantic_tokens_full(
-        &self,
-        _params: SemanticTokensParams,
-    ) -> tower_lsp::jsonrpc::Result<Option<SemanticTokensResult>> {
-        let Some(text) = self.text.lock().await.clone() else {
-            return Ok(None);
-        };
-        let lines = parser::parse_program(&text)
-            .map_err(|_| tower_lsp::jsonrpc::Error::internal_error())?;
+    async fn on_change(&self, text: String) -> Result<()> {
+        *self.text.lock().await = Some(text.clone());
+        let lines = parser::parse_program(&text)?;
 
         let mut tokens = Vec::new();
+
+        let mut label_defs = HashMap::new();
+        let mut label_references = HashMap::new();
 
         let mut last_line = 0;
         for line in lines {
@@ -139,6 +91,8 @@ impl LanguageServer for Lsp {
 
                 last_col = 0;
                 last_line = line.line_no as u32;
+
+                label_defs.insert(label.0.clone(), (line.line_no as u32, label.0.len() as u32));
 
                 tokens.push(token);
             }
@@ -208,6 +162,11 @@ impl LanguageServer for Lsp {
                             token_type: TYPE_MAP[&SemanticTokenType::VARIABLE],
                             token_modifiers_bitset: 0,
                         });
+
+                        label_references
+                            .entry(s.clone())
+                            .or_insert_with(Vec::new)
+                            .push((s_idx, s.len() as u32));
 
                         last_col = s_idx;
                         last_line = line.line_no as u32;
@@ -288,6 +247,82 @@ impl LanguageServer for Lsp {
             }
         }
 
+        *self.semantic_tokens.lock().await = tokens;
+
+        let mut usage_map = self.label_usage_map.lock().await;
+        let mut reference_map = self.label_reference_map.lock().await;
+
+        for (label, (loc, label_len)) in label_defs.iter() {
+            if let Some(references) = label_references.get(label) {
+                let mut reference_locations = Vec::new();
+                for (start, len) in references.iter().copied() {
+                    usage_map.insert(start..start + len, *loc);
+                    reference_locations.push(start);
+                }
+                reference_map.insert(*loc..*loc + *label_len, reference_locations);
+            }
+        }
+
+        Ok(())
+    }
+}
+
+#[tower_lsp::async_trait]
+impl LanguageServer for Lsp {
+    async fn initialize(
+        &self,
+        _: InitializeParams,
+    ) -> tower_lsp::jsonrpc::Result<InitializeResult> {
+        let mut token_types = TYPE_MAP.iter().collect::<Vec<_>>();
+        token_types.sort_by_key(|(_, v)| *v);
+        let token_types = token_types
+            .iter()
+            .map(|(k, _)| (*k).clone())
+            .collect::<Vec<_>>();
+        let mut token_modifiers = MOD_MAP.iter().collect::<Vec<_>>();
+        token_modifiers.sort_by_key(|(_, v)| *v);
+        let token_modifiers = token_modifiers
+            .iter()
+            .map(|(k, _)| (*k).clone())
+            .collect::<Vec<_>>();
+        let tokens =
+            SemanticTokensServerCapabilities::SemanticTokensOptions(SemanticTokensOptions {
+                work_done_progress_options: WorkDoneProgressOptions::default(),
+                legend: SemanticTokensLegend {
+                    token_types,
+                    token_modifiers,
+                },
+                range: None,
+                full: Some(SemanticTokensFullOptions::Bool(true)),
+            });
+        let capabilities = ServerCapabilities {
+            semantic_tokens_provider: Some(tokens),
+            text_document_sync: Some(TextDocumentSyncCapability::Kind(TextDocumentSyncKind::FULL)),
+            ..ServerCapabilities::default()
+        };
+        let result = InitializeResult {
+            capabilities,
+            ..InitializeResult::default()
+        };
+        Ok(result)
+    }
+
+    async fn did_open(&self, params: DidOpenTextDocumentParams) {
+        self.on_change(params.text_document.text).await.unwrap();
+    }
+
+    async fn did_change(&self, params: DidChangeTextDocumentParams) {
+        self.on_change(params.content_changes[0].text.clone())
+            .await
+            .unwrap();
+    }
+
+    async fn semantic_tokens_full(
+        &self,
+        _params: SemanticTokensParams,
+    ) -> tower_lsp::jsonrpc::Result<Option<SemanticTokensResult>> {
+        let tokens = self.semantic_tokens.lock().await.clone();
+
         let result = SemanticTokensResult::Tokens(SemanticTokens {
             result_id: None,
             data: tokens,
@@ -311,6 +346,9 @@ async fn main() -> Result<()> {
     let (service, socket) = LspService::build(|client| Lsp {
         client,
         text: Mutex::new(None),
+        label_usage_map: Mutex::new(RangeMap::new()),
+        label_reference_map: Mutex::new(RangeMap::new()),
+        semantic_tokens: Mutex::new(Vec::new()),
     })
     .finish();
 
